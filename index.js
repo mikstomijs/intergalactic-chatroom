75aedfec197c89e8415613823737dd472b72752c
miks <mikstomijs@gmail.com>
2026-03-16 13:32:29 +0200

    boom

--- index.js
+++ index.js
@@ -4,11 +4,38 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-
+const Database = require('better-sqlite3');
 
 
 app.use(express.static(__dirname));
+
+
+const db = new Database('chat.db');
+
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    room TEXT NOT NULL,
+    nickname TEXT NOT NULL,
+    message TEXT NOT NULL,
+    timestamp INTEGER NOT NULL
+  )
+`);
+
+const getHistory = db.prepare(`
+  SELECT nickname, message 
+  FROM messages 
+  WHERE room = ? 
+  ORDER BY timestamp ASC
+`);
+
+const insertMessage = db.prepare(`
+  INSERT INTO messages (room, nickname, message, timestamp) 
+  VALUES (?, ?, ?, ?)
+`);
+
+
 
 
 
@@ -22,6 +49,11 @@ function setupChatroom(io, room) {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  
+  const history = getHistory.all(room);
+  socket.emit('chat history', history);
+
+
   socket.on('set nickname', (nickname) => {
     socket.nickname = nickname;
     io.emit('chat message', nickname + " joined the chat");
@@ -29,8 +61,11 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', (msg) => {
     const name = socket.nickname || "Anonymous";
+      insertMessage.run(room, name, msg, Date.now());
     io.emit('chat message', name + ": " + msg);
   });
+
+
 
   socket.on('disconnect', () => {
     const name = socket.nickname || "A user";
